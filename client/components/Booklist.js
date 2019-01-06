0b545abe1c4fe669a7b5a38db79729e652f15d19
Chris Li <chrisli2017@gmail.com>
2019-01-06 22:31:28 +0800

    f> filter started

---
+++ client/components/Booklist.js
@@ -0,0 +1,40 @@
+import React from 'react'
+// import { addSearch, removeSearch, apiSearch } from '../store/search'
+import { connect } from 'react-redux'
+
+
+const SearchResults = (props) => {
+    // const onClick = () => {
+
+    // }
+
+  const booklist = props.booklist
+  const numBooks = booklist.numFound
+  return (
+    <React.Fragment>
+      <div>
+        <p>Reul: {numBooks}</p>
+      </div>
+      <div className='bookGrid'>
+        {booklist.docs.map((book) => {
+          const coverArt = book.cover_i
+          const title = book.title
+          const author = book.author_name
+          // const amazonLink = book.id_amazon
+          const coverEdition = cover_edition_key
+          return (
+            <React.Fragment>
+              <img className='bookCover' src={`http://covers.openlibrary.org/b/id/${coverArt}-S.jpg`} />
+              <div>
+                <h2>{title}</h2>
+                <p>By: {author}</p>
+              </div>
+              <button onClick={window.open(`https://openlibrary.org/books/${coverEdition}`)}>Open Library</button>
+              <button onClick={}>Details</button>
+            </React.Fragment>
+          )
+        })}
+      </div>
+    </React.Fragment>
+  )
+}
---
+++ client/components/FilterBar.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { addSearch, removeSearch, apiSearch } from '../store/search'
+import { connect } from 'react-redux'
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props),
+      this.state = {
+        searchCate: 'isbn',
+        searchTerm: ''
+      }
+  }
+
+  handleChange = key => event => {
+    this.setState({
+      [key]: event.target.value
+    })
+    console.log(this.state)
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.props.addSearch(this.state.searchTerm)
+    this.props.apiSearch(this.state.searchCate, this.state.searchTerm)
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <select onChange={this.handleChange('searchCate')}>
+            <option value='isbn'>ISBN</option>
+            <option value='title'>Title</option>
+            <option value='author'>Author</option>
+          </select>
+          <input
+            value={this.state.searchTerm}
+            onChange={this.handleChange('searchTerm')}
+          />
+          <button className='SubmitButton' type='submit'>Submit</button>
+        </form>
+      </div>
+
+    )
+  }
+}
+
+const mapState = state => {
+  return {
+    searchTerm: state.search.searchTerm,
+    searchHistory: state.search.searchHistory
+  }
+}
+
+// const mapDispatch = dispatch => ({
+
+// })
+
+export default connect(mapState)(SearchBar)
